09fb6581b73390a5a7b4e8e215668f0033408df5
Joshva Arockiaraj <JoshvaArockiaraj@users.noreply.github.com>
2024-09-12 16:26:17 +0530

    Process.php got some better readability

--- process.php
+++ process.php
@@ -1,4 +1,4 @@
-<?php ob_start();
+<!-- <?php ob_start();
 	if(isset($_POST['submit'])) {
 	$to = "infantjose2609@gmail.com";
 	$subject = $_POST['subject'];
@@ -12,4 +12,65 @@
 	mail($to,$subject,$message,$headers);
 	header("Location: contact.html");
 	}
+?> -->
+
+<?php
+ob_start();
+ 
+if (isset($_POST['submit'])) {
+    $to = "infantjose2609@gmail.com";
+    $subject = filter_var($_POST['subject'], FILTER_SANITIZE_STRING);
+    $email = filter_var($_POST['email'], FILTER_VALIDATE_EMAIL);
+    $message = htmlspecialchars($_POST['message']);  // Sanitize the message input
+ 
+    if (!$email) {
+        echo "Invalid email format";
+        exit;  // Stop the script if the email is not valid
+    }
+ 
+    // Always set content-type when sending HTML email
+    $headers = "MIME-Version: 1.0" . "\r\n";
+    $headers .= "Content-type: text/html;charset=UTF-8" . "\r\n";
+    $headers .= "From: " . $email . "\r\n";
+ 
+    // Check if the mail was sent successfully
+    if (mail($to, $subject, $message, $headers)) {
+        // If mail is sent, redirect to the contact page
+        header("Location: contact.html");
+        exit();
+    } else {
+        // If the mail fails, print an error message
+        echo "Message could not be sent. Please try again later.";
+    }
+}
+?>
+<?php
+ob_start();
+ 
+if (isset($_POST['submit'])) {
+    $to = "infantjose2609@gmail.com";
+    $subject = filter_var($_POST['subject'], FILTER_SANITIZE_STRING);
+    $email = filter_var($_POST['email'], FILTER_VALIDATE_EMAIL);
+    $message = htmlspecialchars($_POST['message']);  // Sanitize the message input
+ 
+    if (!$email) {
+        echo "Invalid email format";
+        exit;  // Stop the script if the email is not valid
+    }
+ 
+    // Always set content-type when sending HTML email
+    $headers = "MIME-Version: 1.0" . "\r\n";
+    $headers .= "Content-type: text/html;charset=UTF-8" . "\r\n";
+    $headers .= "From: " . $email . "\r\n";
+ 
+    // Check if the mail was sent successfully
+    if (mail($to, $subject, $message, $headers)) {
+        // If mail is sent, redirect to the contact page
+        header("Location: contact.html");
+        exit();
+    } else {
+        // If the mail fails, print an error message
+        echo "Message could not be sent. Please try again later.";
+    }
+}
 ?>
